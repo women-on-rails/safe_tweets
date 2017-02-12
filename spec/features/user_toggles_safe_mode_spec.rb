06c91806564b35c20af1f452eae950c047bd5b92
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-02-13 00:07:33 +0100

    Switch safe / unsafe mode from home page.

---
+++ spec/features/user_toggles_safe_mode_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+
+
+feature "User toggles Safe mode" do
+  let(:message) { FactoryGirl.create(:message) }
+  let(:troll_message) { FactoryGirl.create(:troll_message) }
+
+  scenario "it shows unsafe tweets" do
+    visit root_path
+    # This is not working. Why????
+    # click_on(unsafe_messages_path)
+    visit unsafe_messages_path
+    expect(page).to have_content(troll_message.author)
+  end
+
+end
