f14187ae69f0a984b1332187ad042cc5ee92df98
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-02-24 16:18:56 +0100

    Added some style to buttons.

--- spec/features/user_changes_msg_safe_status.rb
+++ spec/features/user_changes_msg_safe_status.rb
@@ -4,12 +4,12 @@ feature 'User changes msg safe status' do
 
   scenario 'finds the safe link' do
     visit_unsafe_path
-    page.find_by_id("message_#{message.id}").find("a.unsafe")
+    page.find_by_id("message_#{message.id}").find("a.is_unsafe")
   end
 
   scenario 'clicking on safe link' do
     visit_unsafe_path
-    page.find_by_id("message_#{troll_message.id}").find("a.safe").click
+    page.find_by_id("message_#{troll_message.id}").find("a.is_safe").click
     troll_message.reload
     expect(page).not_to have_selector(:css, "#message_#{troll_message.id}")
     expect(troll_message.safe).to be true
@@ -17,12 +17,12 @@ feature 'User changes msg safe status' do
 
   scenario 'finds the unsafe link' do
     visit_unsafe_path
-    page.find_by_id("message_#{troll_message.id}").find("a.unsafe")
+    page.find_by_id("message_#{troll_message.id}").find("a.is_unsafe")
   end
 
   scenario 'cliking on unsafe link' do
     visit_unsafe_path
-    page.find_by_id("message_#{message.id}").find("a.unsafe").click
+    page.find_by_id("message_#{message.id}").find("a.is_unsafe").click
     message.reload
     expect(page).not_to have_selector(:css, "#message_#{message.id}")
     expect(message.safe).to be false
@@ -30,7 +30,7 @@ feature 'User changes msg safe status' do
 
   scenario 'finds the unsafe link anywhere' do
     visit_safe_path
-    page.find_by_id("message_#{message.id}").find("a.unsafe")
+    page.find_by_id("message_#{message.id}").find("a.is_unsafe")
   end
 
 end
